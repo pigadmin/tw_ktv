6d605267fc78a3e9237978815d63c31ce6ef4283
zhu <964182377@qq.com>
2018-07-11 18:07:34 +0800

    test

--- app/src/main/java/com/ktv/ui/fragments/subFragments/RankList.java
+++ app/src/main/java/com/ktv/ui/fragments/subFragments/RankList.java
@@ -17,7 +17,6 @@ import android.widget.TextView;
 
 import com.google.gson.reflect.TypeToken;
 import com.ktv.R;
-import com.ktv.adapters.RankListAda;
 import com.ktv.adapters.RankListAdapter;
 import com.ktv.adapters.base.RecyclerAdapter;
 import com.ktv.app.App;
